0f0a7595098fd76468abf487425053269dd06366
vivisgalvis <vivigalvis.89@gmail.com>
2020-11-25 08:28:16 -0500

    Creacion de menu home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from '../src/Components/Menu/Menu';
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
+    
     <div className="App">
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+         <div><Menu /></div>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
