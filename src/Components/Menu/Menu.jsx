1bb33cb1978d045ab2bea0219cfb2b3b8edd7ce5
vivisgalvis <vivigalvis.89@gmail.com>
2020-11-26 11:14:37 -0500

    Cambios de estilo menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import '../Menu/Menu.css';
 import  Agregar from '../../../src/images/Agregar.png';
 import  Buscar from '../../../src/images/Buscar.png';
-import  Casos from '../../../src/images/Casos.png';
 import  Home from '../../../src/images/Home.png';
 
 const NavBar = () => {
@@ -19,10 +18,9 @@ const NavBar = () => {
             </label>
 
             <ul className="menu-options">
+                <li><a href=""><img src={Home} alt={"Home"}/></a></li>
                 <li><a href="/"><img src={Agregar} alt={"Agregar"}/></a></li>
                 <li><a href=""><img src={Buscar} alt={"Buscar"}/></a></li>
-                <li><a href=""><img src={Casos} alt={"Casos"}/></a></li>
-                <li><a href=""><img src={Home} alt={"Home"}/></a></li>
             </ul>
         </nav>
     </div>
