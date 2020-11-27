70c39bdb5eac4b332f7f67f43fa05ce8b3765fb3
danielablancom <blancodanielac@gmail.com>
2020-11-27 13:07:53 -0300

    preview view with styles

--- src/Components/Opinion/TableCase.jsx
+++ src/Components/Opinion/TableCase.jsx
@@ -1,43 +1,43 @@
-import React from "react";
+// import React from "react";
 
 
-function TableCase() {
+// function TableCase() {
   
-  return (
-    <div className="App">
+//   return (
+//     <div className="App">
       
-        <div className ="container-table">
-          <p className ="title-cases">Casos</p>
-    <table class="table">
-      <thead>
-        <tr>
+//         <div className ="container-table1">
+//           <p className ="title-cases">Casos</p>
+//     <table class="table">
+//       <thead>
+//         <tr>
       
-          <th scope="col">Fecha</th>
-          <th scope="col">Nombre</th>
-          <th scope="col">Tema</th>
-          <th scope="col">Area Cliente</th>
-          <th scope="col">Area Relacionada</th>
-          <th scope="col">Opiniones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
+//           <th scope="col">Fecha</th>
+//           <th scope="col">Nombre</th>
+//           <th scope="col">Tema</th>
+//           <th scope="col">Area Cliente</th>
+//           <th scope="col">Area Relacionada</th>
+//           <th scope="col">Opiniones</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
    
-          <td>09/10/2020</td>
-          <td>Proyecto Fotovoltaico</td>
-          <td>Proyectos; Ambiental; Regulación</td>
-          <td>Gerencia de Construcción</td>
-          <td>Gerencia de Ingeniería; Gerenciad de Medio Ambiente; Gerencia de Negocios</td>
-          <td> </td>
-        </tr>
+//           <td>09/10/2020</td>
+//           <td>Proyecto Fotovoltaico</td>
+//           <td>Proyectos; Ambiental; Regulación</td>
+//           <td>Gerencia de Construcción</td>
+//           <td>Gerencia de Ingeniería; Gerenciad de Medio Ambiente; Gerencia de Negocios</td>
+//           <td> </td>
+//         </tr>
         
        
-      </tbody>
-    </table>
-    </div>
+//       </tbody>
+//     </table>
+//     </div>
  
 
-    </div>
-  );
-}
-export default TableCase;
+//     </div>
+//   );
+// }
+// export default TableCase;
